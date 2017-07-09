52048b2cfd61a29b78ed4ae8111df89845a908fd
Gurunath Pai <pai.gurunath@gmail.com>
2017-07-10 01:24:15 +0530

    working code to some extinct

--- secure_single_v2.js
+++ secure_single_v2.js
@@ -3,7 +3,8 @@ var noble = require('noble');
 // Search only for the Service UUID of the device (remove dashes)
 var IMU_SERVICE_UUID = '917649a0d98e11e59eec0002a5d5c51b';
 
-var uid_single = ['917649a1d98e11e59eec0002a5d5c51b']
+var uid_single1 = ['917649a1d98e11e59eec0002a5d5c51b']
+var uid_single2 = ['917649a2d98e11e59eec0002a5d5c51b']
 
 
 	// start scanning when bluetooth is powered on
@@ -28,12 +29,15 @@ try {
 	    console.log('Connected to peripheral: ' + peripheral.uuid);
 	    peripheral.discoverServices([IMU_SERVICE_UUID], function(error, services) {
 	      var imuService = services[0];
+
 	      console.log('Discovered IMU service');
 		console.log("imuService : " + imuService);
 		
-	      imuService.discoverCharacteristics(uid_single, function(error, characteristics) {
+	      imuService.discoverCharacteristics(uid_single1, function(error, characteristics) {
 
 	        var axCharacteristic = characteristics[0];
+
+	        console.log("axCharacteristic" + axCharacteristic);
 
 			axCharacteristic.on('read', function(data, isNotification) {
 			  console.log( "Ax : " + data.readFloatLE(0) + "Ay : " + data.readFloatLE(4) + "Az : " + data.readFloatLE(8));
@@ -44,6 +48,25 @@ try {
 	    	});
 
 	      });
+
+
+	      imuService.discoverCharacteristics(uid_single2, function(error, characteristics) {
+
+	        var gxCharacteristic = characteristics[0];
+
+	        console.log("gxCharacteristic" + gxCharacteristic);
+
+			gxCharacteristic.on('read', function(data, isNotification) {
+			  console.log( "gx : " + data.readFloatLE(0) + "gy : " + data.readFloatLE(4) + "gz : " + data.readFloatLE(8));
+			});
+	        // to enable notify
+	        gxCharacteristic.subscribe(function(error) {
+	          console.log('GX notification on');
+	    	});
+
+	      });
+
+
 	    });
 	  });
 	});
